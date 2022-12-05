2764b4371c49f885ba57cee4e7421b2f221a7611
unknown <Dmis0474@gmail.com>
2022-12-05 21:59:52 +0300

    profile has added

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,12 +1,86 @@
 import React from "react";
-import styles from "./blog.module.css"
+import styles from "./blog.module.css";
+import { useEffect, useState } from "react";
 
 function Blog() {
-    return (
-      <div className={styles.blog}>
-        <h1>Блог находится в разработке</h1>
-      </div>
-    );
-  }
+
+  const [blogItems, setBlogItems] = useState([]);
+
+
+  useEffect(() => {
+    fetch('https://fakerapi.it/api/v1/images?_quantity=10&_height=150&_width=150')
+	.then(response => response.json())
+  .then(response => setBlogItems(response.data))
+  // .then(response => console.log(response))
+  .then(console.log(blogItems))
+	.catch(err => console.error(err));
+  }, []);
+
   
-  export default Blog;
+
+  return (
+    <div className={styles.blog}>
+      <div className={styles.name}>
+        <img alt="ava" src="ava-test.jpg" className={styles.ava} />
+        <div className={styles.nameItems}>
+          <div className={styles.nickWrapper}>
+            <p className={styles.nick} onClick={()=>{console.log(blogItems)}}>Марина-марина</p>
+          </div>
+          <div className={styles.audience}>
+            <p className={styles.audienceCount}>подписчики : 123</p>
+            <p className={styles.audienceCount}>подписки : 123</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.about}>
+        <div className={styles.description}>
+          <p className={styles.descriptionName}>Марина Ерохина</p>
+          <p className={styles.descriptionText}>
+            Я известный блогер миллионер нумеролог и провидец в 8 поколении, мои
+            хобби это скалолазанье, прыжки с паращьютом, полеты в космос и на
+            луну
+          </p>
+        </div>
+        <div className={styles.links}>
+          <div className={styles.nickWrapper}>
+            <p className={styles.nick}>Редактировать</p>
+          </div>
+          <ul className={styles.networks}>
+            <li className={styles.networkItemWrapper}>
+              <p className={`${styles.networkItem} ${styles.ig}`}>instagramm</p>
+            </li>
+            <li className={styles.networkItemWrapper}>
+              <p className={`${styles.networkItem} ${styles.tg}`}>telegramm</p>
+            </li>
+            <li className={styles.networkItemWrapper}>
+              <p className={`${styles.networkItem} ${styles.tw}`}>twitter</p>
+            </li>
+            <li className={styles.networkItemWrapper}>
+              <p className={`${styles.networkItem} ${styles.wp}`}>what's upp</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.timeline}>
+        <div className={styles.addPostBlock}>
+          <input placeholder="Введите что у вас происходит..." />
+          <button>Опубликовать</button>
+        </div>
+      </div>
+      <div className={styles.blogList}>
+      {blogItems.map((item) => (
+        <div key={item.title} className={styles.blogPost}>
+            <h3>{item.title}</h3>
+            <div className={styles.postWrapper}>
+            <img alt="postImg" src={`${item.url}`}></img>
+            <p>{item.description}</p>
+            </div>
+        </div>
+        
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Blog;
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import styles from "./profile.module.css"
+import styles from "./profile.module.css";
 
-function Profile () {
-    return (
-      <div className={styles.profile}>
-        <h1>Раздел "Профиль" находится в разработке</h1>
-      </div>
-    );
-  }
-  
-  export default Profile;
+function Profile() {
+  return (
+    <div className={styles.profile}>
+      <img alt="ava" src="ava-test.jpg" className={styles.ava} />
+      <h2 className={styles.name}>Марина-марина</h2>
+      <ul className={styles.profileItems}>
+        <li className={styles.profileItem}>Мои друзья</li>
+        <li className={styles.profileItem}>Мои посты</li>
+        <li className={styles.profileItem}>Сохраненные посты</li>
+        <li className={styles.profileItem}>Изменить профиль</li>
+        <li className={styles.profileItem}>Настройки приложения</li>
+        <li className={styles.profileItem}>Гороском на сегодня</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Profile;
